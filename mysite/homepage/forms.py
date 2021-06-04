c4a67ad9b93f2233aa928728ce670f28b80dd30d
Viram Jain <viramjain9742@gmail.com>
2021-06-04 11:16:35 +0530

    Final Commit

---
+++ mysite/homepage/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from . import models
+
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = models.UserDetails
+        fields = ["email", "password"]
+        widgets = {
+            "email": forms.EmailInput(),
+            "password": forms.PasswordInput(),
+        }
